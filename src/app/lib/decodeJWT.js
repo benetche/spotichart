dae95a6a39c3111994c30c07b068beef7ac48168
Vítor Beneti <vitor.beneti@gmail.com>
2025-01-21 00:17:31 -0300

    fix cookies

--- src/app/lib/decodeJWT.js
+++ src/app/lib/decodeJWT.js
@@ -1,8 +1,16 @@
 import { decode } from "next-auth/jwt";
-export const dynamic = "force-dynamic";
 
 async function decodeJWT(cookieStore) {
-  const token = cookieStore.get("authjs.session-token");
+  let token = undefined;
+  if (process.env.NODE_ENV === "development") {
+    token = cookieStore.get("authjs.session-token");
+  } else {
+    token = cookieStore.get("__Secure-authjs.session-token");
+  }
+
+  if (!token) {
+    throw new Error("Token not found");
+  }
   const decrypted = await decode({
     token: token.value,
     salt: token.name,
